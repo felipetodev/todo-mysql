6ab59d8eb23aa735aad4cb1fd06644341504e373
felipetodev <fe.ossandon.u@gmail.com>
2024-06-13 02:30:15 -0400

    fix: add header gh repo link

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,10 +1,18 @@
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+
 export function Header() {
   return (
     <nav className="h-[80px] bg-primary px-6 flex justify-between items-center">
       <h1 className="text-white text-4xl font-bold">
         To Do List
       </h1>
-      <a>
+      <a
+        rel="noreferrer noopener"
+        href="https://github.com/felipetodev/transvip-chall"
+        target="_blank"
+        className={cn(buttonVariants({ variant: "outline" }))}
+      >
         GitHub
       </a>
     </nav>
