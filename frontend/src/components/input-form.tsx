00ede163463cb3bacd9cb89536e110cbbd1bc2f3
felipetodev <fe.ossandon.u@gmail.com>
2024-06-13 00:20:00 -0400

    feat: add input to create todos

--- frontend/src/components/input-form.tsx
+++ frontend/src/components/input-form.tsx
@@ -1,14 +1,40 @@
+import { useRef } from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 
 export function InputForm() {
+  const inputRef = useRef<HTMLInputElement>(null)
   return (
-    <form className="flex space-x-4 p-5 w-full max-w-3xl mt-4">
+    <form
+      onSubmit={e => e.preventDefault()}
+      className="flex space-x-4 p-5 w-full max-w-3xl mt-4"
+    >
       <Input
         name="todo"
+        ref={inputRef}
+        autoComplete="off"
         placeholder="¿Qué te gustaría realizar?"
       />
-      <Button>
+      <Button
+        onClick={() => {
+          if (!inputRef.current?.value) return alert("Por favor, ingrese una tarea")
+          try {
+            fetch("http://localhost:3000/todos", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json"
+              },
+              body: JSON.stringify({
+                description: inputRef.current.value,
+              })
+            })
+          } catch (e) {
+            alert(e)
+          } finally {
+            inputRef.current.value = ""
+          }
+        }}
+      >
         AGREGAR
       </Button>
     </form>
