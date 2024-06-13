3bf0c5ac9577d8a4736ee17e30db5406a772b2d4
felipetodev <fe.ossandon.u@gmail.com>
2024-06-12 23:03:35 -0400

    feat: add header component

---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <nav className="h-[80px] bg-primary px-6 flex justify-between items-center">
+      <h1 className="text-white text-4xl font-bold">
+        To Do List
+      </h1>
+      <a>
+        GitHub
+      </a>
+    </nav>
+  )
+}
